6405e964d3355eb15b5919424a98de9231d4ba9a
Raisson Souza <raissonrai@gmail.com>
2025-02-06 11:47:09 -0300

    Estilo dark mapeado e organizado.

--- data/style.ts
+++ data/style.ts
@@ -45,32 +45,32 @@ const DarkStyle: Style = {
     ...defaultStyle,
     styleName: "dark",
 
-    backgroundColor: "#fff",
+    backgroundColor: "#fff", // branco
 
-    headerBackgroundColor: "#00008B",
-    headerTextColor: "#fff",
+    headerBackgroundColor: "#00008B", // darkblue
+    headerTextColor: "#fff", // branco
 
-    footerBackgroundColor: "#00008B",
-    footerSelectedTabBackgroundColor: "#4169E1",
-    footerIconColor: "#fff",
-    footerTextColor: "#fff",
+    footerBackgroundColor: "#00008B", // darkblue
+    footerSelectedTabBackgroundColor: "#4169E1", // royalblue
+    footerIconColor: "#fff", // branco
+    footerTextColor: "#fff", // branco
 
-    btnImportantBackgroundColor: "#00008B",
-    btnOutlineColor: "#38B4E1",
-    btnBackgroundColor: "#fff",
+    btnImportantBackgroundColor: "#00008B", // darkblue
+    btnOutlineColor: "#38B4E1", // lightblue
+    btnBackgroundColor: "#fff", // branco
 
-    textColor: "#000",
-    inactiveTextColor: "#808080",
-    oppositeTextColor: "#fff",
+    textColor: "#000", // black
+    inactiveTextColor: "#808080", // gray
+    oppositeTextColor: "#fff", // branco
 
-    iconColor: "#000",
-    inactiveIconColor: "#808080",
-    oppositeIconColor: "#fff",
+    iconColor: "#000", // black
+    inactiveIconColor: "#808080", // gray
+    oppositeIconColor: "#fff", // branco
 
-    primary: "#00008B",
-    secondary: "#4169E1",
-    terciary: "#12518E",
-    quaternary: "#d3d3d3",
+    primary: "#00008B", // darkblue
+    secondary: "#4169E1", // royalblue
+    terciary: "#12518E", // outro azul escuro
+    quaternary: "#d3d3d3", // lightgray
 }
 
 const LightStyle: Style = {
