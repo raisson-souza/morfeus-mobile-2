bcb70332d3ff334d1664a5ea6cdf7f9f557a72fa
Raisson Souza <raissonrai@gmail.com>
2025-02-06 14:44:07 -0300

    Correção de comportamento e tela de carregamento de StyleContext.

--- contexts/StyleContext.tsx
+++ contexts/StyleContext.tsx
@@ -1,7 +1,9 @@
+import { ActivityIndicator, View } from "react-native"
+import { CustomImage } from "@/components/customs/CustomImage"
 import { DarkStyle, LightStyle } from "@/data/style"
 import { Style } from "@/types/style"
 import { useSQLiteContext } from "expo-sqlite"
-import DefaultLoadingScreen from "@/components/screens/general/DefaultLoadingScreen"
+import Box from "@/components/base/Box"
 import React, { createContext, useContext, useEffect, useState } from "react"
 
 type StyleContextProps = {
@@ -40,7 +42,7 @@ export default function StyleContextComponent({ children }: StyleContextProps) {
         setSystemStyle(_style === "dark" ? DarkStyle : LightStyle)
     }
 
-    if (loading) return <DefaultLoadingScreen />
+    if (loading) return customLoadingScreen(systemStyle.loadingColor, systemStyle.largeIconSize)
 
     return (
         <StyleContext.Provider value={{
@@ -57,3 +59,30 @@ export function StyleContextProvider() {
     if (!context) throw new Error("StyleContext chamado fora do provider.")
     return context
 }
+
+/** Componente especial de tela de carregamento para StyleContext */
+const customLoadingScreen = (loadingColor: string, iconSize: number) => <View
+    style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        flex: 1,
+    }}
+>
+    <Box.Column
+        style={{
+            alignItems: "center",
+            gap: 15,
+        }}
+    >
+        <CustomImage.Local filePathByRequire={ require('../assets/images/morfeus_logo.png') } />
+        <View>
+        <ActivityIndicator
+            color={ loadingColor }
+            size={ iconSize }
+        />
+    </View>
+    </Box.Column>
+</View>
